ecf4c4e77bc382deb8cb293107ac9a7c429de58f
Newland-A <ajn252@gmail.com>
2020-11-06 12:21:09 -0500

    remove example nested attribute writer

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -7,16 +7,9 @@ class Animal < ApplicationRecord
     company_attrs.values.each do |animal_attr|
       if company_attrs("location").present?
         company = Company.find_or_create_by(animal_attr)
-        self.animals << 
-
-  def categories_attributes=(cats_attrs)
-    cats_attrs.values.each do |cat_attr|
-      if cat_attr["name"].present?
-        category = Category.find_or_create_by(cat_attr)
-        self.categories << category
+        self.animals << company
       end
     end
   end
-
 
 end
