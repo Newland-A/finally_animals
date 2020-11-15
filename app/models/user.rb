d3676cb70d6a835879dc7ce640252355446a4c4e
Newland-A <ajn252@gmail.com>
2020-11-14 22:49:30 -0500

    added methods to the user model to keep code more organized

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,26 @@ class User < ApplicationRecord
   validates :username, :email, presence: true, uniqueness: true
   validates_length_of :password, minimum: 8, if: Proc.new { | user | user.password.present? }
 
+  def self.create_by_google_omniauth(auth)
+    self.find_or_create_by(username: auth[:info][:email]) do |u|
+      u.id = User.last.id + 1
+      u.email = auth[:info][:email]
+      u.first_name = auth[:info][:first_name]
+      u.last_name = auth[:info][:last_name]
+      u.password = SecureRandom.hex
+    end
+  end
+
+  def self.create_by_github_omniauth(auth)
+    self.find_or_create_by(username: auth[:info][:email]) do |u|
+    u.id = User.last.id + 1
+    u.email = auth[:info][:email]
+    u.first_name = auth[:info][:first_name]
+    u.last_name = auth[:info][:last_name]
+    u.password = SecureRandom.hex
+    end
+  end
+
   def full_name
     "#{self.first_name}" + " #{self.last_name.capitalize}"
   end
