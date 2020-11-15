28824b0e0322641431ec0696786a2878801d0e8a
Newland-A <ajn252@gmail.com>
2020-11-14 22:50:39 -0500

    changed route to be more generic instead of having multiple get methods for the providers ie google and github for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],
-  provider_ignores_state: true
-  provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'],
-  provider_ignores_state: true
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
-  provider_ignores_state: true
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-  provider_ignores_state: true
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
+  
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], provider 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'home#index'
-  
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :users
-  get 'index', to: 'users#index', as: 'index'
-  resources :animals
-  delete '/animals/:id', to: "users#destroy"
-  # resources :companies
-
-  # resources :animals do
-  #   resources :companies
-  # end
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create' 
   get 'logout', to: 'sessions#destroy', as: 'logout'
-  get '/auth/facebook/callback', to: 'sessions#create_with_omni'
-  get '/auth/github/callback', to: 'sessions#create_with_omni'
-  get '/auth/google/callback', to: 'sessions#create_with_omni'
 
-  # scope :animals do 
-  #   resources :users
-  # end
+  get '/auth/:provider/callback', to: 'sessions#create_with_omni'
+  
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users do
+    resources :animals
+  end
+
+  resources :animals 
+
 end
