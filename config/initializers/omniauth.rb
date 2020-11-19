1fe5d5766a92018d51c7fae44d80b7886917a517
Newland-A <ajn252@gmail.com>
2020-11-18 22:06:12 -0500

    forced the omniauth hash to accept the email attribute

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :info_fields => 'email'
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
