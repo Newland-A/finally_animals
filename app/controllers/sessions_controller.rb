d49e01d51238671f061d120b60211be401dd88f1
Newland-A <ajn252@gmail.com>
2020-11-25 06:28:36 -0500

    change route path after action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,24 +2,12 @@ class SessionsController < ApplicationController
 
   def new
   end
-
-  def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
-    else
-      flash.now[:alert] = "Email or password is invalid"
-      render "new"
-    end
-  end
   
   def create_with_omni
     if params[:provider] == 'google_oauth2'
       @user = User.create_by_google_omniauth(auth)
       session[:user_id] = @user.id
       redirect_to @user
-
     elsif params[:provider] == 'github'
       @user = User.create_by_github_omniauth(auth)
       session[:user_id] = @user.id
@@ -35,17 +23,11 @@ class SessionsController < ApplicationController
         redirect_to root_url, notice: "Logged in!"
       else
         flash[:error] = "Sorry, login info was incorrect. Please try again."
-        redirect_to login_path
+        render login_path
       end
     end
   end
 
-  def omniauth
-    @user = User.create_by_google_omniauth(auth)
-    session[:user_id] = @user.id
-    redirect_to @user
-  end
-  
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
